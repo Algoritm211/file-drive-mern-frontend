8284b1482bb995573f9b17e2de18177dce25a88a
alexey_horbunov <algoritm211@gmail.com>
2021-02-27 00:59:13 +0200

    Add deploy settings v5

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
-const express = require('express');
-const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || 8080;
-
-// здесь у нас происходит импорт пакетов и определяется порт нашего сервера
+const express = require('express');
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.png'));
-
-//здесь наше приложение отдаёт статику
-app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
-
-//простой тест сервера
-app.get('/ping', function (req, res) {
-  return res.send('pong');
+const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
+app.use(express.static(publicPath));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(publicPath, 'index.html'));
 });
-
-//обслуживание html
-app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+app.listen(port, () => {
+  console.log('Server is up!');
 });
-app.listen(port);
